42cd09b897c1aa25596b07b63bf19090e4fd865e
Michał Bednarczyk <michal.bednarczyk@g2a.com>
2021-10-27 12:20:21 +0200

    feat: created products-grid layout

--- src/features/layout/layout.component.tsx
+++ src/features/layout/layout.component.tsx
@@ -4,7 +4,7 @@ export const Layout = ({ children }: any) => {
   return (
     <>
       <Navbar />
-      <main className="container py-4">{children}</main>
+      <main className="py-4">{children}</main>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Button } from "react-bootstrap";
 import { store } from "./app/store";
 import { Layout } from "./features/layout/layout.component";
+import { ProductsGrid } from "./features/products-grid/products-grid.component";
 import "./app/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Layout>
-        <Button>Hello</Button>
+        <ProductsGrid />
       </Layout>
     </Provider>
   </React.StrictMode>,
